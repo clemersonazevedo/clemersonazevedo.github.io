1c64904afe92b397c34918efb2e691590930c96e
Clemerson Azevedo <clemerson.azevedo@gmail.com>
2021-06-18 17:20:22 -0300

    teste

--- oplab/assets/js/price.js
+++ oplab/assets/js/price.js
@@ -3,7 +3,7 @@
         document.getElementById("idFree").innerHTML = '<h4 class="old"><sup>&nbsp;</sup></h4><h4><sup>R$</sup><span>0</span><sup>/mês</sup></h4>';
         document.getElementById("idFit").innerHTML = '<h4 class="old"><sup>R$47/mês</sup></h4><h4><sup>R$</sup><span>37</span><sup>/mês</sup></h4>';
         document.getElementById("idMax").innerHTML = '<h4 class="old"><sup>R$159/mês</sup></h4><h4><sup>R$</sup><span>119</span><sup>/mês</sup></h4>';
-        document.getElementById("idPro").innerHTML = '<h4 class="old"><sup>&nbsp;</sup></h4><h4><sup>sob consulta</sup></h4>';
+        // document.getElementById("idPro").innerHTML = '<h4 class="old"><sup>&nbsp;</sup></h4><h4><sup>sob consulta</sup></h4>';
         document.getElementById("idBtnMensal").className = 'ativo';
         document.getElementById("idBtnAnual").className = 'desativo';
       }
@@ -11,7 +11,7 @@
         document.getElementById("idFree").innerHTML = '<h4 class="old"><sup>&nbsp;</sup></h4><h4><sup>R$</sup><span>0</span><sup>/mês</sup></h4>';
         document.getElementById("idFit").innerHTML = '<h4 class="old"><sup>R$444/mês</sup></h4><h4><sup>R$</sup><span>370</span><sup>/mês</sup></h4>';
         document.getElementById("idMax").innerHTML = '<h4 class="old"><sup>R$1.428/mês</sup></h4><h4><sup>R$</sup><span>1.190</span><sup>/mês</sup></h4>';
-        document.getElementById("idPro").innerHTML = '<h4 class="old"><sup>&nbsp;</sup></h4><h4><sup>sob consulta</sup></h4>';
+        // document.getElementById("idPro").innerHTML = '<h4 class="old"><sup>&nbsp;</sup></h4><h4><sup>sob consulta</sup></h4>';
         document.getElementById("idBtnMensal").className = 'desativo';
         document.getElementById("idBtnAnual").className = 'ativo';
       }
